0ce053808f4f4bd2c20971f6ad62be7dac55d298
Loic Duros <loic.duros@gmail.com>
2018-11-08 11:29:24 -0500

    added nested user in views for historical records

--- quartet_trail/serializers.py
+++ quartet_trail/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    '''
+    User Serializer for task history.
+    '''
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
 
 class QuartetTrailSerializer(serializers.ModelSerializer):
 
+    history_user = UserSerializer(read_only=True)
+    
     class Meta:
         model = None
         fields = '__all__'
